aa0fd79eee29abc44d7d1abb7e378023b5556906
Evgeny Yakimov <john.yakimov@gmail.com>
2021-04-16 08:27:22 +0100

    More complex use of trace

--- src/example.cpp
+++ src/example.cpp
@@ -1,6 +1,16 @@
 #include "example.h"
+#include <opentelemetry/trace/provider.h>
+
+namespace {
+auto get_tracer()
+{
+    return opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("example");
+}
+}
 
 int example()
 {
+    auto tracer = get_tracer();
+    tracer->StartSpan("example", { { "answer", 42 } }, { { tracer->GetCurrentSpan()->GetContext(), {{"trueParent", true}} } });
     return 42;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,40 @@
 #include "example.h"
 
+#include "opentelemetry/exporters/ostream/span_exporter.h"
 #include "opentelemetry/sdk/trace/simple_processor.h"
 #include "opentelemetry/sdk/trace/tracer_provider.h"
 #include "opentelemetry/trace/provider.h"
-#include "opentelemetry/exporters/ostream/span_exporter.h"
 
-
+#include <chrono>
 #include <iostream>
 
-namespace
-{
+namespace {
 void initTracer()
 {
-  auto exporter = std::unique_ptr<sdktrace::SpanExporter>(
-      new opentelemetry::exporter::trace::OStreamSpanExporter);
-  auto processor = std::shared_ptr<sdktrace::SpanProcessor>(
-      new sdktrace::SimpleSpanProcessor(std::move(exporter)));
-  auto provider = nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
-      new sdktrace::TracerProvider(processor, opentelemetry::sdk::resource::Resource::Create({}),
-                                   std::make_shared<opentelemetry::sdk::trace::AlwaysOnSampler>()));
+    auto exporter = std::unique_ptr<sdktrace::SpanExporter>(
+        new opentelemetry::exporter::trace::OStreamSpanExporter);
+    auto processor = std::shared_ptr<sdktrace::SpanProcessor>(
+        new sdktrace::SimpleSpanProcessor(std::move(exporter)));
+    auto provider = nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
+        new sdktrace::TracerProvider(processor, opentelemetry::sdk::resource::Resource::Create({}),
+            std::make_shared<opentelemetry::sdk::trace::AlwaysOnSampler>()));
 
-  // Set the global trace provider
-  opentelemetry::trace::Provider::SetTracerProvider(provider);
+    // Set the global trace provider
+    opentelemetry::trace::Provider::SetTracerProvider(provider);
 }
-}  // namespace
 
-int main() {
+auto get_tracer()
+{
+    return opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("main");
+}
+
+} // namespace
+
+int main()
+{
+    initTracer();
+    auto tracer = get_tracer();
+    auto span = tracer->StartSpan("main", { { "key", "val" } });
+    auto scope = tracer->WithActiveSpan(span);
     std::cout << "Hello World: " << example() << std::endl;
 }
