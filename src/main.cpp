fe616ab0b4ebcfe848a8f0be2e772db442b85f57
Evgeny Yakimov <john.yakimov@gmail.com>
2021-05-04 21:08:40 +0100

    Add test lib dependency

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include "opentelemetry/sdk/trace/tracer_provider.h"
 #include "opentelemetry/trace/provider.h"
 
+#include <otel-cpp-lib-test/hello.h>
+
+#include <dlfcn.h>
+
 #include <chrono>
 #include <iostream>
 #include <memory>
@@ -31,11 +35,38 @@ opentelemetry::nostd::shared_ptr<opentelemetry::trace::Tracer> get_tracer()
 
 } // namespace
 
-int main()
+int main(int argc, char* argv[])
 {
     initTracer();
     auto tracer = get_tracer();
     auto span = tracer->StartSpan("main", { { "key", "val" } });
     auto scope = tracer->WithActiveSpan(span);
     std::cout << "Hello World: " << example() << std::endl;
+    hello();
+
+    if (argc < 2) {
+        return 0;
+    }
+
+    void* handle = dlopen(argv[1], RTLD_LAZY);
+    if (!handle) {
+        std::cerr << "Cannot open library: " << dlerror() << std::endl;
+        return 1;
+    }
+
+    dlerror(); // reset errors
+
+    // load the symbol
+    typedef void (*world_t)();
+    world_t world = (world_t)dlsym(handle, "world");
+    const char* dlsym_error = dlerror();
+    if (dlsym_error) {
+        std::cerr << "Cannot load symbol 'world': " << dlsym_error << std::endl;
+        return 1;
+    }
+
+    world();
+
+    // close the library
+    dlclose(handle);
 }
